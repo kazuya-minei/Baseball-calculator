0ffb1365ee2dfb229004bff79d2a0bcb80bcf59f
kazuya-minei <clannad1009@gmail.com>
2021-05-28 17:15:41 +0900

    ♻️ 打率処理をリファクタリング

--- src/components/Functions/BattingAverage.jsx
+++ src/components/Functions/BattingAverage.jsx
@@ -6,22 +6,21 @@ export function BattingAvarage() {
   let [BA, setBA] = useState("打率0割0分0厘");
   
   const getAvarage = useCallback(() => {
-    function getNumResult(params) {
-      const bats = document.getElementById('bats').value;  // 打席数
-      const hits = document.getElementById('hits').value;  // 安打数
-      const avarage = hits/bats;  // 計算
-      const res = Math.round(avarage * 1000)/1000;  // 四捨五入
-      const SetResult = String(res); // 文字列変換
-      return Array.from(SetResult);  // 配列変換
-    }
-    let NumResult = getNumResult();
-    if (NumResult[0] === "1") {
+    const bats = document.getElementById('bats').value;  // 打席数
+    const hits = document.getElementById('hits').value;  // 安打数
+    const avarage = hits/bats;  // 計算
+    const res = Math.round(avarage * 1000)/1000;  // 四捨五入
+    const NumResult = String(res); // 文字列変換
+    if (NumResult.slice(0, 1) === "1") {
       setBA(`10割打者!? 首位打者は君だ!`);
-    } else if (!NumResult[4]) {
-      setBA(`打率${NumResult[2]}割${NumResult[3]}分`);
+    } else if (NumResult.length <= 3) {
+      // 2文字目から初めて、最初から3文字目までを抜き出す
+      setBA(`打率 ${NumResult.slice(2, 3)} 割`)
+    } else if (NumResult.length <= 4) {
+      setBA(`打率 ${NumResult.slice(2, 3)}割${NumResult.slice(3, 4)}分 `)
     } else {
-      setBA(`打率${NumResult[2]}割${NumResult[3]}分${NumResult[4]}厘`);
-    };
+      setBA(`打率 ${NumResult.slice(2, 3)}割${NumResult.slice(3, 4)}分${NumResult.slice(4, 5)}厘`)
+    }
   }, []);
 
   return (
